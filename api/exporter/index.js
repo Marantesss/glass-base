25305f1fff2ae49dba5061300189f1d59ca13232
Marantesss <up201706917@fe.up.pt>
2021-02-04 14:02:53 +0000

    :sparkles: finished exporter for contracts and participating entities

--- api/exporter/index.js
+++ api/exporter/index.js
@@ -1,20 +1,26 @@
 const fs = require('fs')
 const knex = require('../knex')
 
+const objectArrayToCSV = (arr) => {
+  // create column names from object keys
+  const columnNames = Object.keys(arr[0])
+  // create array of arrays from object values
+  const columnValues = arr.map((row) => Object.values(row))
+  // concat with contract objects
+  const csvArray = [...[columnNames], ...columnValues]
+  // join everything with ',' and '\n'
+  const csvString = csvArray.map((row) => row.join(',')).join('\n')
+
+  return csvString
+}
+
 const contractsToCSV = async (numberOfContracts = 1000, filename = 'contracts.csv') => {
   // fetch contracts
   const contracts = await knex('contract')
     .orderBy('publicationDate', 'desc')
     .limit(numberOfContracts)
 
-  // create column names from object keys
-  const columnNames = Object.keys(contracts[0])
-  // create array of arrays from object values
-  const columnValues = contracts.map((row) => Object.values(row))
-  // concat with contract objects
-  const csvArray = [...[columnNames], ...columnValues]
-  // join everything with ',' and '\n'
-  const csvString = csvArray.map((row) => row.join(',')).join('\n')
+  const csvString = objectArrayToCSV(contracts)
 
   // write fo file
   try {
@@ -22,11 +28,52 @@ const contractsToCSV = async (numberOfContracts = 1000, filename = 'contracts.cs
   } catch (error) {
     console.error(error)
   }
+
+  return contracts
 }
 
+const entitiesContractedToCSV = async (contracts, filename = 'entitiesContracted.csv') => {
+  const contractIds = contracts.map((contract) => contract.id)
+  // fetch all entities contracted in these contracts
+  const entities = await knex('entityContracted')
+    .whereIn('contractId', contractIds)
+    .join('entity', 'entityContracted.entityNif', 'entity.nif')
+
+  const csvString = objectArrayToCSV(entities)
+
+  // write fo file
+  try {
+    fs.writeFileSync(filename, csvString)
+  } catch (error) {
+    console.error(error)
+  }
+
+  return entities
+}
+
+const entitiesContractorToCSV = async (contracts, filename = 'entitiesContractor.csv') => {
+  const contractIds = contracts.map((contract) => contract.id)
+  // fetch all entities contracted in these contracts
+  const entities = await knex('entityContractor')
+    .whereIn('contractId', contractIds)
+    .join('entity', 'entityContractor.entityNif', 'entity.nif')
+
+  const csvString = objectArrayToCSV(entities)
+
+  // write fo file
+  try {
+    fs.writeFileSync(filename, csvString)
+  } catch (error) {
+    console.error(error)
+  }
+
+  return entities
+}
 
 const main = async () => {
-  await contractsToCSV()
+  const contracts = await contractsToCSV()
+  await entitiesContractedToCSV(contracts)
+  await entitiesContractorToCSV(contracts)
 
   return 'Success'
 }
