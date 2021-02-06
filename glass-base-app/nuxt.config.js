6b3bb01286a626685d7ccb467094d478960de350
Marantesss <up201706917@fe.up.pt>
2021-02-06 18:20:48 +0000

    :lipstick: add tailwind configuration file and remove vue style overrides

--- glass-base-app/nuxt.config.js
+++ glass-base-app/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'glass-base-app',
+    title: 'Glass Base',
     htmlAttrs: {
       lang: 'en',
     },
@@ -13,11 +13,20 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      // favicon
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // Inter
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~assets/css/main'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
---
+++ glass-base-app/tailwind.config.js
@@ -0,0 +1,44 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      serif: ['Merriweather', ...defaultTheme.fontFamily.serif],
+      mono: defaultTheme.fontFamily.mono,
+    },
+    extend: {
+      colors: {
+        // Main purple and lighter variants
+        'glass-purple': '#4453C3',
+        'glass-purple-light-1': '#6975CF',
+        'glass-purple-light-2': '#8F98DB',
+        'glass-purple-light-3': '#B4BAE7',
+        'glass-purple-light-4': '#DADDF3',
+        'glass-purple-light-5': '#ECEEF9',
+        // blue
+        'glass-blue': '#00509D',
+      },
+      textColor: {
+        dark: '#0A0A16', // black
+        light: '#E5E5E5', // white
+        'gray-light': '#A6B0C4', // light grey
+        'gray-dark': '#585D6D', // dark grey
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    content: [
+      'components/**/*.{vue,js}',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.{js,ts}',
+      'nuxt.config.{js,ts}',
+      'content/**/*.md',
+    ],
+  },
+}
