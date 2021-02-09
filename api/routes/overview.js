2a3b87ca17f4b5bdd4f104c06b383a5d90dbad60
Marantesss <up201706917@fe.up.pt>
2021-02-09 00:45:40 +0000

    :sparkles: :zap: add all queries for overview + async for better performance

--- api/routes/overview.js
+++ api/routes/overview.js
@@ -11,81 +11,104 @@ router.get('/', async (req, res) => {
   const startDate = parseDateString(req.query.startDate) || new Date('2020-01-01')
   const endDate = parseDateString(req.query.endDate) || new Date('2020-12-31')
 
-  // TODO: perform queries asynchronously
+  // perform queries asynchronously
+  const [
+    contractCount,
+    contractValue,
+    contractAvgValue,
+    contractsNumberOverTime,
+    contractsValueOverTime,
+    contractTypes,
+    topContractedNumber,
+    topContractedValue,
+    topContractorsNumber,
+    topContractorsValue,
+  ] = await Promise.all([
+    // BIG NUMBERS
+    knex('contract')
+      .count('id as contractCount')
+      .first(),
+    // https://stackoverflow.com/questions/13113096/how-to-round-an-average-to-2-decimal-places-in-postgresql/20934099
+    // double precision
+    knex('contract')
+      .select(knex.raw('ROUND(SUM("initialContractualPrice"::double precision)::numeric, 2) as "contractValue"'))
+      .first(),
+    // TODO: 3798 contracts with initialContractualPrice = 0
+    knex('contract')
+      .select(knex.raw('ROUND(AVG("initialContractualPrice")::numeric, 2) as "contractAvgValue"'))
+      .first(),
 
-  // BIG NUMBERS
-  const { contractCount } = await knex('contract')
-    .count('id as contractCount')
-    .first()
-  // https://stackoverflow.com/questions/13113096/how-to-round-an-average-to-2-decimal-places-in-postgresql/20934099
-  // double precision
-  const { contractValue } = await knex('contract')
-    .select(knex.raw('ROUND(SUM("initialContractualPrice"::double precision)::numeric, 2) as "contractValue"'))
-    .first()
-  // TODO: 3798 contracts with initialContractualPrice = 0
-  const { contractAvgValue } = await knex('contract')
-    .select(knex.raw('ROUND(AVG("initialContractualPrice")::numeric, 2) as "contractAvgValue"'))
-    .first()
+    // CONTRACTS OVER TIME
+    knex('contract')
+      .select(
+        knex.raw('EXTRACT(MONTH from "publicationDate") AS "month"'),
+        knex.raw('EXTRACT(YEAR from "publicationDate") AS "year"'),
+      )
+      .count('id as value')
+      .whereBetween('publicationDate', [startDate, endDate])
+      .groupBy('month', 'year')
+      .orderBy('month', 'asc'),
+    knex('contract')
+      .select(
+        knex.raw('EXTRACT(MONTH from "publicationDate") AS "month"'),
+        knex.raw('EXTRACT(YEAR from "publicationDate") AS "year"'),
+        knex.raw('ROUND(SUM("initialContractualPrice"::double precision)::numeric, 2) as "value"'),
+      )
+      .whereBetween('publicationDate', [startDate, endDate])
+      .groupBy('month', 'year')
+      .orderBy('month', 'asc'),
 
-  // Contracts over time
-  const contractsOverTime = await knex('contract')
-    .select('publicationDate')
-    .count('publicationDate as value')
-    .where('publicationDate', '>=', startDate)
-    .andWhere('publicationDate', '<=', endDate)
-    .groupBy('publicationDate')
-    .orderBy('publicationDate', 'asc')
+    // CONTRACT TYPES
+    knex('contract')
+      .select('contractingProcedureType')
+      .count('contractingProcedureType as value')
+      .groupBy('contractingProcedureType')
+      .orderBy('value', 'desc')
+      .limit(6),
 
-  // Contract Types
-  const contractTypes = await knex('contract')
-    .select('contractingProcedureType')
-    .count('contractingProcedureType as value')
-    .groupBy('contractingProcedureType')
-    .orderBy('value', 'desc')
-    .limit(6)
+    // TOP ENTITIES
+    knex('entity')
+      .select('entity.nif', 'entity.description as name')
+      .count('entityContracted.entityNif as value')
+      .join('entityContracted', 'entityContracted.entityNif', 'entity.nif')
+      .groupBy('entity.nif')
+      .orderBy('value', 'desc')
+      .limit(6),
+    knex('entity')
+      .select('entity.nif', 'entity.description as name',
+        knex.raw('ROUND(SUM("contract"."initialContractualPrice"::double precision)::numeric, 2) as "value"'))
+      .join('entityContracted', 'entityContracted.entityNif', 'entity.nif')
+      .join('contract', 'contract.id', 'entityContracted.contractId')
+      .groupBy('entity.nif')
+      .orderBy('value', 'desc')
+      .limit(6),
 
-  // TOP ENTITIES
-  const topContractedNumber = await knex('entity')
-    .select('entity.nif', 'entity.description')
-    .count('entityContracted.entityNif as value')
-    .join('entityContracted', 'entityContracted.entityNif', 'entity.nif')
-    .groupBy('entity.nif')
-    .orderBy('value', 'desc')
-    .limit(6)
-  const topContractedValue = await knex('entity')
-    .select('entity.nif', 'entity.description',
-      knex.raw('ROUND(SUM("contract"."initialContractualPrice"::double precision)::numeric, 2) as "value"'))
-    .join('entityContracted', 'entityContracted.entityNif', 'entity.nif')
-    .join('contract', 'contract.id', 'entityContracted.contractId')
-    .groupBy('entity.nif')
-    .orderBy('value', 'desc')
-    .limit(6)
-
-  const topContractorNumber = await knex('entity')
-    .select('entity.nif', 'entity.description')
-    .count('entityContractor.entityNif as value')
-    .join('entityContractor', 'entityContractor.entityNif', 'entity.nif')
-    .groupBy('entity.nif')
-    .orderBy('value', 'desc')
-    .limit(6)
-  const topContractorValue = await knex('entity')
-    .select('entity.nif', 'entity.description',
-      knex.raw('ROUND(SUM("contract"."initialContractualPrice"::double precision)::numeric, 2) as "value"'))
-    .join('entityContractor', 'entityContractor.entityNif', 'entity.nif')
-    .join('contract', 'contract.id', 'entityContractor.contractId')
-    .groupBy('entity.nif')
-    .orderBy('value', 'desc')
-    .limit(6)
+    knex('entity')
+      .select('entity.nif', 'entity.description as name')
+      .count('entityContractor.entityNif as value')
+      .join('entityContractor', 'entityContractor.entityNif', 'entity.nif')
+      .groupBy('entity.nif')
+      .orderBy('value', 'desc')
+      .limit(6),
+    knex('entity')
+      .select('entity.nif', 'entity.description as name',
+        knex.raw('ROUND(SUM("contract"."initialContractualPrice"::double precision)::numeric, 2) as "value"'))
+      .join('entityContractor', 'entityContractor.entityNif', 'entity.nif')
+      .join('contract', 'contract.id', 'entityContractor.contractId')
+      .groupBy('entity.nif')
+      .orderBy('value', 'desc')
+      .limit(6),
+  ])
 
   res.json({
     status: 'OK',
     contractCount,
     contractValue,
     contractAvgValue,
-    contractsOverTime,
     contractTypes,
-    topContracted: { topContractedNumber, topContractedValue },
-    topContractor: { topContractorNumber, topContractorValue },
+    contractsOverTime: { number: contractsNumberOverTime, value: contractsValueOverTime },
+    topContracted: { number: topContractedNumber, value: topContractedValue },
+    topContractors: { number: topContractorsNumber, value: topContractorsValue },
   })
 })
 
