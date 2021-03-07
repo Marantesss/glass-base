f00588a0f649f2775227c4dada7c74efb9bcd321
Marantesss <up201706917@fe.up.pt>
2021-03-07 20:02:28 +0000

    :poop: :rocket: :bug:

--- api/knexfile.js
+++ api/knexfile.js
@@ -1,4 +1,9 @@
+const pg = require('pg')
 const { database } = require('./config.js')
+
+pg.defaults.ssl = {
+  rejectUnauthorized: false,
+}
 
 module.exports = {
   development: {
@@ -22,6 +27,7 @@ module.exports = {
     client: database.client,
     connection: database.database_url,
     ssl: { rejectUnauthorized: false },
+    // ssl: 'no-verify', // try this if above does not work for you
     migrations: {
       tableName: 'knex_migrations',
     },
